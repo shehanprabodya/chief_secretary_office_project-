a13f097b34181a1803d872105c919dc961f0a5cf
shehanprabodya <shehanprabodya870@gmail.com>
2026-06-09 14:11:39 +0530

    create header component

---
+++ frontend/src/components/layouts/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return(
+        <header>
+            <div className="log">
+                <img src="" alt="" />
+            </div>
+            <div className="actions">
+                <button>🔔</button>
+                <button>👤</button>
+            </div>
+        </header>
+
+        
+
+    );
+};
+
+export default Header;
